3678afc29b6d58593a22463c62e4d4f007e81e63
ROBOT55 <robot55+gitConfig@gmail.com>
2018-06-21 20:17:51 +0300

    added json form api and character in model and some vue stuff

---
+++ heromakerClient/main.js
@@ -0,0 +1,59 @@
+//something
+var app = new Vue({
+  el: '#app',
+  data: {
+    abilities: [
+      {
+			"name": "loading...",
+			"url": "http://www.dnd5eapi.co/api/ability-scores/1"
+		}
+    ],
+    selectedAbility: false
+  },
+  methods: {
+    getAbilities: function () {
+      	jQuery.getJSON( "http://www.dnd5eapi.co/api/ability-scores/", function( json ) {
+	  		app.abilities = json.results;
+
+	  		jQuery.each(app.abilities,function( i ) {	// for every ability in abilities, call me back with its index
+
+  				app.hydrateObject(app.abilities[i],function(hydratedObject){ // call hydtate with an ability and a callback for the hydrated object
+	  				app.abilities[i] = hydratedObject // <<-- Essentially THIS is the callback 
+	  				app.selectedAbility = app.abilities[i];
+
+	  			});
+
+	  			app.character.abilities[app.abilities[i].name] = d20.roll('3d6')
+
+			});
+
+
+
+		});
+    },
+    // I get an object and a callback.
+    // I callback with the hydratedObject
+    hydrateObject: function (object, callback) {
+    	jQuery.getJSON( object.url, callback);
+    },
+    selectAbility: function (ability){
+    	console.log(ability)
+    	app.selectedAbility=ability
+    },
+
+    createBlankCharacter: function (){
+    	app.character = {
+    		abilities:{}
+    	}
+    }
+
+
+
+
+  }
+})
+
+$( document ).ready(function() {
+    app.createBlankCharacter ();
+    app.getAbilities ();
+});
